bfb9e5de396e16e87d4c45dbee37b0f1a227abb4
Gundeep Singh <gundeepsinghm@gmail.com>
2023-10-01 22:28:02 +0530

    x

---
+++ guan/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Restaurant from "./components/Basics/Restaurants";
+
+const App = () => {
+  return <Restaurant />
+  
+};
+
+export default App;
---
+++ guan/src/components/Basics/menuApi.js
@@ -0,0 +1,77 @@
+const Menu = [
+    {
+        id: 1,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+
+    {
+        id: 2,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+    {
+        id: 3,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+    {
+        id: 4,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+    {
+        id: 5,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+    {
+        id: 6,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+    {
+        id: 7,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+    {
+        id: 8,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+    {
+        id: 9,
+        image: "images/safkdsf.jpg",
+        name: "Candle",
+        category: "Mould",
+        price: 100,
+        description: "Lorem Ipsum"
+    },
+];
+
+export default Menu;
